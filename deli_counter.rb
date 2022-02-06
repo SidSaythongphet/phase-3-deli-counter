b8588a7edcc524ff78803e2c1468f0f88b3fdc03
Sid Saythongphet <sid.saythongphet@gmail.com>
2022-02-06 16:01:31 -0500

    commit tree ac3d9182b34d1f9cda77f9e03382f1e1a27a7525

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+katz_deli = []
+
+def line arr
+    queue = arr.map.with_index { |name, index| " #{ index + 1 }. #{ name }" }.join
+    if arr.length == 0 then puts "The line is currently empty."
+    else puts "The line is currently:#{ queue }"
+    end
+end
+
+
+def take_a_number arr, customer 
+    arr << customer
+    puts "Welcome, #{ customer }. You are number #{ arr.length } in line."
+end 
+
+
+def now_serving arr
+    if !arr[0] then puts "There is nobody waiting to be served!"
+    else puts "Currently serving #{ arr[0] }."
+    end
+    arr.shift
+end
+    
+line katz_deli
+now_serving katz_deli
+take_a_number(katz_deli, "Ada")
+take_a_number(katz_deli, "Grace")
+    
+line katz_deli
+now_serving katz_deli
